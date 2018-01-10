2e9226a0c7e59796284f08198108b88e116d8323
wangdi <chenghao.wd@alibaba-inc.com>
2018-01-10 10:17:08 +0800

    1

--- src/main/java/org/sprintdragon/odps/demo/MyDomain.java
+++ src/main/java/org/sprintdragon/odps/demo/MyDomain.java
@@ -1,39 +1,34 @@
 package org.sprintdragon.odps.demo;
 
+import com.aliyun.odps.io.IntWritable;
+import com.aliyun.odps.io.Text;
 import com.aliyun.odps.io.Writable;
+import lombok.Data;
+import lombok.ToString;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class MyDomain implements Writable{
+@Data
+@ToString
+public class MyDomain implements Writable {
 
-    private String groupId;
-    private String ip;
+    private IntWritable type;
+    private Text groupId;
+    private Text ip;
 
-    public String getGroupId() {
-        return groupId;
+    @Override
+    public void write(DataOutput out) throws IOException {
+        type.write(out);
+        groupId.write(out);
+        ip.write(out);
     }
 
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public void write(DataOutput dataOutput) throws IOException {
-        dataOutput.writeChars(groupId);
-        dataOutput.writeChars(ip);
-    }
-
-    public void readFields(DataInput dataInput) throws IOException {
-        groupId = dataInput.readLine();
-        ip = dataInput.readLine();
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        type.readFields(in);
+        groupId.readFields(in);
+        ip.readFields(in);
     }
 }
--- src/main/java/org/sprintdragon/odps/demo/UdafDemoContains.java
+++ src/main/java/org/sprintdragon/odps/demo/UdafDemoContains.java
@@ -1,26 +1,78 @@
 package org.sprintdragon.odps.demo;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aliyun.odps.io.IntWritable;
+import com.aliyun.odps.io.LongWritable;
+import com.aliyun.odps.io.Text;
 import com.aliyun.odps.io.Writable;
 import com.aliyun.odps.udf.Aggregator;
-import com.aliyun.odps.udf.UDF;
 import com.aliyun.odps.udf.UDFException;
+import lombok.Data;
+import lombok.ToString;
 
-public class UdafDemoContains extends Aggregator{
+public class UdafDemoContains extends Aggregator {
 
-
+    @Override
     public Writable newBuffer() {
-        return null;
+        return new MyDomainListBuffer();
     }
 
+    @Override
     public void iterate(Writable writable, Writable[] writables) throws UDFException {
-
+        IntWritable type = (IntWritable)writables[0];
+        Text groupId = (Text)writables[1];
+        Text ip = (Text)writables[2];
+        MyDomain myDomain = new MyDomain();
+        myDomain.setType(type);
+        myDomain.setGroupId(groupId);
+        myDomain.setIp(ip);
+        MyDomainListBuffer buffer = (MyDomainListBuffer)writable;
+        buffer.add(myDomain);
     }
 
+    @Override
     public Writable terminate(Writable writable) throws UDFException {
-        return null;
+        MyDomainListBuffer result = (MyDomainListBuffer)writable;
+        LongWritable longWritable = new LongWritable();
+        longWritable.set(IpUtils.countRongYu(result.getList()));
+        return longWritable;
     }
 
-    public void merge(Writable writable, Writable writable1) throws UDFException {
-
+    @Override
+    public void merge(Writable writable0, Writable writable1) throws UDFException {
+        MyDomainListBuffer result = (MyDomainListBuffer)writable0;
+        MyDomainListBuffer buffer = (MyDomainListBuffer)writable1;
+        result.addBuffer(buffer);
     }
+}
+
+@Data
+@ToString
+class MyDomainListBuffer implements Writable {
+    private List<MyDomain> list = new ArrayList<MyDomain>();
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        for (MyDomain acl : list) {
+            acl.write(out);
+        }
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+    }
+
+    public void add(MyDomain myDomain) {
+        list.add(myDomain);
+    }
+
+    public void addBuffer(MyDomainListBuffer myDomainListBuffer) {
+        list.addAll(myDomainListBuffer.getList());
+    }
+
 }
